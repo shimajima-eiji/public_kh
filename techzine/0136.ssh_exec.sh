2087f3e75353df7ada98d761b91dbdc61309bd7a
shimajima-eiji <shimajima-eiji@live.jp>
2018-09-07 19:41:20 +0900

    0136

---
+++ techzine/0136.ssh_exec.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+# apt install zip
+# apt install sshpass / 
+# need authorized key(all servers)
+
+user='user'
+pass='pass'
+port=port
+server_head='server'
+server_foot='_operation'
+
+cmd=$1
+output=${2:-/dev/null}
+
+log_cmd=$(echo "${cmd}" | sed 's/ /_/g')
+if [ ! "${output}" = '/dev/null' ]; then
+  sav_log="$(dirname $0)/$(date "+%Y%m%d_%H%M%S")-${log_cmd}"
+  mkdir ${sav_log}
+fi
+
+for i in `seq 1 19`
+do
+  if [ $i -eq 1 -a "$cmd" = "df" ]; then
+    continue
+  fi
+  server=`printf "${server_head}%02d${server_foot}" $i`
+  echo ${server}  # for debug, or(and) monitoring
+  if [ ! "${output}" = '/dev/null' ]; then
+    output="${sav_log}/${server}_${log_cmd}.log"
+  fi
+  echo "${pass}" | sshpass -p "${pass}" ssh -p ${port} -t -o "StrictHostKeyChecking no" $user@${server} "sudo -S ${cmd}" 2>/dev/null | tee ${output}
+done
+
+if [ ! "${output}" = '/dev/null' ]; then
+  zip -r ${sav_log}.zip ${sav_log}
+  rm -rf ${sav_log}
+fi
+||<
+
+** cron
+>|sh|
+0 */1 * * * sh ~/ssh_exec.sh "top -b -n 1" cron
+0 */1 * * * sh ~/ssh_exec.sh "free -m" cron
+0 */1 * * * sh ~/ssh_exec.sh "ps aux" cron
+0 */1 * * * sh ~/ssh_exec.sh "df" cron
+
+*/1 * * * * mkdir -p ~/$(date "+\%Y/\%m/\%d")
+59 23 */1 * *  find ~/ -name "*$(date '+\%Y\%m\%d')*" -exec mv {} ~/$(date '+\%Y/\%m/\%d') \\; 2>>~/cron.log
+||<
