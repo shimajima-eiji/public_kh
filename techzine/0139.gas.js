80bae8e758868fa81c769b9f958ddb8f8ea336c3
shimajima-eiji <shimajima-eiji@live.jp>
2018-09-11 13:15:42 +0900

    RaspberryPi3,GrovePi+ temperature,humidity sensor

--- techzine/0139.gas.js
+++ techzine/0139.gas.js
@@ -5,15 +5,15 @@ function doPost(e){
    *    temperature(float): xx.x
    *    humidity (float)  : xx.x
    */
-  var params = e.parameter;  # from sensor.py's post
-  var reg_server_time = Utilities.formatDate(new Date(),"JST","yyyyMMdd HHmmss");  # JavaScriptで日付フォーマットを扱う場合、Utilitiesで行う
+  var params = e.parameter;  // from sensor.py's post
+  var reg_server_time = Utilities.formatDate(new Date(),"JST","yyyyMMdd HHmmss");  // JavaScriptで日付フォーマットを扱う場合、Utilitiesで行う
   var reg_client_time = params.datetime;
   var temperature = params.temperature;
   var humidity = params.humidity;
 
   result = call_add_TH([reg_server_time, reg_client_time, temperature, humidity]);
   message = result === true ? 'regist successful' : '[FAILED] regist data' + e;
-  return ContentService.createTextOutput(message);  # ここはPython側でやりようがあるはず。returnの値が異常値だった場合に再送信するとか
+  return ContentService.createTextOutput(message);  // ここはPython側でやりようがあるはず。returnの値が異常値だった場合に再送信するとか
 }
 
 function call_add_TH(array) {
