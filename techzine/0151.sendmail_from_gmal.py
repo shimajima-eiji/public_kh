13bf8f97b5e0eebc576846d9ca260bea6826d4ea
shimajima-eiji <shimajima-eiji@live.jp>
2018-09-14 18:06:26 +0900

    [py2] smtp server gmail

---
+++ techzine/0151.sendmail_from_gmal.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import gmail
+
+# Login to Gmail settings
+GMAIL_USER = '(user)'
+GMAIL_PASS = '(pass)'
+
+# Mail parameter
+TOADDR = '(slackbot@workspace.slack.com)'
+
+subject = '日本語'  # Japanese
+body = 'メッセージ\n改行も'  # Message, and new line
+message = gmail.Message(subject, to=TOADDR , text=body)
+
+# send mail
+client = gmail.GMail(GMAIL_USER , GMAIL_PASS)
+client.send(message)
+client.close()
