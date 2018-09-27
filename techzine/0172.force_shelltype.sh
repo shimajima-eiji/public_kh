d6672531c7d9c626a76bf65cc98f3c17e72119dc
shimajima-eiji <shimajima-eiji@live.jp>
2018-09-27 16:57:56 +0900

    refactoring force_shelltype

--- techzine/0172.force_shelltype.sh
+++ techzine/0172.force_shelltype.sh
@@ -1,18 +1,26 @@
-#!/bin/bash
+force_shelltype() {
+  force=$1
 
-force='bash'
-### filter
-# not bash case, force bash
-p=$(ps --pid $$ -o command | tail -1 | cut -f1 -d' ')
-if [ ! "${p}" = "${force}" ] ; then
-  if [ "$(which bash)" = '' ] ; then
-    echo "${force}"': not installed.'
-    exit 1
+  # not case, force sh
+  p=$(ps --pid $$ ho command | awk '{print $1}')
+  if [ ! "${p}" = "${force}" ] ; then
+    check_sh="$(echo ${force} | grep sh)"
+    if [ "${check_sh}" = '' ] ; then
+      echo "${sh_type}: not sh"
+      exit 1
+    elif [ "$(which ${check_sh} 2>/dev/null | grep sh)" = '' ] ; then
+      echo "${sh_type}"': not installed.'
+      exit 1
+    else
+      # memo: $@=${@:1}
+      $1 ${0:@}
+      exit 0
+    fi
   fi
+}
 
-  bash ${0} "$@"
-  exit 0
-fi
+sh_type=$1
+force_shelltype ${sh_type}
 
-# force bash
-some commands...
+# Expected Result: sh ${0}
+ps --pid $$ ho pid,command
