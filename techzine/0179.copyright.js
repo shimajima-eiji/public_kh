2a395dbfb41fb03391599c5d2228d2c2380d5fbc
shimajima-eiji <shimajima-eiji@live.jp>
2018-10-04 09:42:43 +0900

    auto domain name printer

---
+++ techzine/0179.copyright.js
@@ -0,0 +1,22 @@
+/** sample
+<!-- Copyright -->
+<span id="copyright"></span>
+*/
+
+$(function(write_date){
+  var article = new Date(write_date);
+  var y = article.getFullYear();
+  
+  /** 欲しいのはここまで。以下、参考情報
+  var m = article.getMonth() + 1;
+  var d = article.getDate();
+  var w = article.getDay();
+  var wd = ['日', '月', '火', '水', '木', '金', '土'];
+  var h = article.getHours();
+  var mi = article.getMinutes();
+  var s = article.getSeconds();
+  // 参考情報　ここまで */
+  
+  // id:cpに張り付ける
+  $('#copyright').text("Copyright: https://nomuraya.work/ since " + y);
+});
