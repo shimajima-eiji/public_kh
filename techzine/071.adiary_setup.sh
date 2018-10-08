cb80c0909a4b2fda437c7540baf6d797f50f8dc4
shimajima-eiji <shimajima-eiji@live.jp>
2018-10-09 08:13:44 +0900

    fix comment

--- techzine/071.adiary_setup.sh
+++ techzine/071.adiary_setup.sh
@@ -1,2 +1,2 @@
-#move to
-#https://github.com/shimajima-eiji/mylib/blob/master/adiary/setup.sh
+# move to: https://github.com/shimajima-eiji/mylib/blob/master/adiary/setup.sh
+# usage: https://nomuraya.work/techzine/071
