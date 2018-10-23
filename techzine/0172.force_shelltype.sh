aa0c003ab0331a829f2de09dfbd68f161620d5e0
shimajima-eiji <shimajima-eiji@live.jp>
2018-10-23 14:47:06 +0900

    smally

--- techzine/0172.force_shelltype.sh
+++ techzine/0172.force_shelltype.sh
@@ -1,26 +1,11 @@
 force_shelltype() {
-  force=$1
-
-  # not case, force sh
   p=$(ps --pid $$ ho command | awk '{print $1}')
-  if [ ! "${p}" = "${force}" ] ; then
-    check_sh="$(echo ${force} | grep sh)"
-    if [ "${check_sh}" = '' ] ; then
-      echo "${sh_type}: not sh"
-      exit 1
-    elif [ "$(which ${check_sh} 2>/dev/null | grep sh)" = '' ] ; then
-      echo "${sh_type}"': not installed.'
-      exit 1
-    else
-      # memo: $@=${@:1}
-      $1 ${0:@}
-      exit 0
-    fi
+  if [ ! "${p}" = 'bash' ] ; then
+    bash $0 $@
+    exit 0
   fi
 }
+force_shelltype $@
 
-sh_type=$1
-force_shelltype ${sh_type}
-
-# Expected Result: sh ${0}
-ps --pid $$ ho pid,command
+p=$(ps --pid $$ ho command | awk '{print $1}')
+echo ${p} $@
